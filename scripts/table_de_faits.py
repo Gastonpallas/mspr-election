b274ee637d63a5baf5a23f6cad8157c433e49a79
phearoak <phearoak@gmail.com>
2024-02-09 10:38:35 +0100

    add: script of table de faits

---
+++ scripts/table_de_faits.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+population = pd.read_excel('./data_raw/Populations.xlsx')
+departement = pd.read_csv('./data_raw/departements-france.csv')
+output_path = './data_clean/population_departement.csv'
+df = pd.merge(population, departement, on='code_departement', how='inner')
+df = df[['code_departement', 'nom_departement', 'Total']]
+df = df.rename(columns={'Total': 'total_population'})
+df.to_csv(output_path, index=False)
