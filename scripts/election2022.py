2ab877c4ba94f4b4af221d35a84bf4c501c29d17
Gaston Pallas <pallasgaston@gmail.com>
2024-03-18 12:45:34 +0100

    election2022

--- scripts/election2022.py
+++ scripts/election2022.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 df = pd.read_csv('https://www.data.gouv.fr/fr/datasets/r/12591a2e-3735-4471-959b-69782eb0f62f')
 csv = df.to_csv()
-output_path = './data_clean/election2022.csv'
+output_path = '../donnees/election2022.csv'
 
 column_rm = ['prenom', 'libelle_departement']
 df = df.drop(columns=column_rm)
@@ -21,15 +21,18 @@ df = df.drop('nom', axis=1)
 
 df.drop(df[(df['code_departement'] == '986') | (df['code_departement'] == '987') | (df['code_departement'] == '988') | (df['code_departement'] == '989')].index, inplace=True)
 
-df.to_csv(output_path, index=False) 
 
 df_pivot = df.pivot(index='code_departement', columns='parti_politique', values='voix').reset_index()
 
 df_pivot.columns.name = None
 df_pivot = df_pivot.rename_axis(None, axis=1)
 
-df_pivot['gagnant'] = df_pivot[['LREM', 'RN', 'LFI', 'Reconuête', 'EELV', 'Résistons', 'LR', 'PCF', 'PS', 'DLF', 'NPA', 'LO', 'abstentions', 'blancs', 'nuls']].idxmax(axis=1)
+df_pivot['gagnant'] = df_pivot[['LREM', 'RN', 'LFI', 'Reconuête', 'EELV', 'Résistons', 'LR', 'PCF', 'PS', 'DLF', 'NPA', 'LO']].idxmax(axis=1)
 
-df_pivot.to_csv("/Users/sacha/Documents/Cours Epsi 2023/MSPR1/reformatage_Données/Testrandom.csv", index=False)
+df_pivot.to_csv(output_path, index=False)
 
 print(df_pivot)
+
+#%%
+
+#%%
