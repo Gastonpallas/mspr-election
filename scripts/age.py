2fc340748edbe71be3a06c1a4b616d59c74ecda7
Gaston Pallas <pallasgaston@gmail.com>
2024-03-18 10:48:16 +0100

    age reformate

--- scripts/age.py
+++ scripts/age.py
@@ -5,5 +5,11 @@ df = pd.read_excel('./data_raw/age.xlsx')
 column = ['ZONE', 'SEXE', 'TRAGE', 'POP_2022']
 df = df[column]
 
+df_pivot = df.pivot(index=['code_departement', 'SEXE'], columns='TRAGE', values='POP_2022').reset_index()
+df_pivot.columns.name = None
+df = df_pivot
+
+df.drop(columns=['SEXE'], inplace=True)
+
 output_path = './data_clean/age.csv'
 df.to_csv(output_path, index=False)
