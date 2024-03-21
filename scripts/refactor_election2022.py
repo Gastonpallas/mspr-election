dae4a68d6a5fb08671d619626ce41c4eccc68c47
phearoak <phearoak@gmail.com>
2024-03-21 10:35:10 +0100

    fix security scripts

--- scripts/refactor_election2022.py
+++ scripts/refactor_election2022.py
@@ -5,14 +5,13 @@ csv = df.to_csv()
 output_path = './data_refactor/election2022.csv'
 
 df = df.loc[df['code_departement'] != 'fr_etranger']
-
 correspondance_partis = {
     'nom': ['MACRON', 'LE PEN', 'MÉLENCHON', 'ZEMMOUR', 'JADOT', 'LASSALLE', 'PÉCRESSE', 'ROUSSEL', 'HIDALGO', 'DUPONT-AIGNAN', 'POUTOU', 'ARTHAUD', 'abstentions', 'blancs', 'nuls'],
     'parti_politique': ['LREM', 'RN', 'LFI', 'Reconuête', 'EELV', 'Résistons', 'LR', 'PCF', 'PS', 'DLF', 'NPA', 'LO', 'abstentions', 'blancs', 'nuls']
 }
 
 df_correspondance = pd.DataFrame(correspondance_partis)
-df = pd.merge(df, df_correspondance, on='nom', how='left')
+df = pd.merge(df, df_correspondance, on='parti_politique', how='left')
 df = df.drop('nom', axis=1)
 
 df.drop(df[(df['code_departement'] == '986') | (df['code_departement'] == '987') | (df['code_departement'] == '988') | (df['code_departement'] == '989')].index, inplace=True)
@@ -26,5 +25,3 @@ df_pivot = df_pivot.rename_axis(None, axis=1)
 df_pivot['gagnant'] = df_pivot[['LREM', 'RN', 'LFI', 'Reconuête', 'EELV', 'Résistons', 'LR', 'PCF', 'PS', 'DLF', 'NPA', 'LO']].idxmax(axis=1)
 
 df_pivot.to_csv(output_path, index=False)
-
-print(df_pivot)
